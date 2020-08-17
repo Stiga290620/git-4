09f54242e539b6e443ace3403e638f026aa841ed
Stiga290620 <arch20des@mail.rus>
2020-08-17 19:14:44 +0300

    v0 19-10

---
+++ f5.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import http.client
+
+conn = http.client.HTTPSConnection("covid-19-coronavirus-statistics.p.rapidapi.com")
+
+
+headers = {
+    'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
+    'x-rapidapi-key': "---"
+}
+
+conn.request("GET", "/v1/stats?country=", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+s = data.decode()
+
+
+spis = s.split('country')
+for i in range(len(spis)):
+    s = spis[i]
+    spis[i] = s.split(',')
+print(len(spis))
+for i in range(len(spis)):
+    print(len(spis[i]))
+
+myfile0 = open('eggs2.txt', 'w')
+#with myfile0:
+
+myfile0.write('":"Country"'+ '\n')
+myfile0.write('"lastUpdatee":"2020-03-15T18:20:18"'+ '\n')
+myfile0.write('"c-onfirmed":'+ '\n')
+myfile0.write('"d-eeathss":'+ '\n')
+myfile0.write('"r-eccovereds":'+ '\n')
+myfile0.write('"p-roovinces":"'+ '\n')
+myfile0.write('"'+ '\n')
+myfile0.write('==================0'+ '\n')
+
+
+for i in range(len(spis)):
+    if i!=0:
+        for ii in range(len(spis[i])):
+            myfile0.write(spis[i][ii]+ '\n')
+        myfile0.write('==================0'+ '\n')
+#myfile0.write(data.decode("utf-8"))
+#print((data.decode("utf-8")))
